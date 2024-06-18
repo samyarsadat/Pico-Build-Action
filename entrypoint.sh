aa60dc24ceca55f001b29e125bae84c6222988c8
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2024-06-18 05:00:05 +0300

    Added tree.

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,6 +87,7 @@ echo "MAKEFILES_GENERATOR=$MAKEFILES_GENERATOR"
 echo "Generating build files..."
 mkdir "$OUTPUT_DIR" && cd "$OUTPUT_DIR"
 cmake -DPICO_BOARD="$BOARD_NAME" -S "$SOURCE_DIR" -B "$OUTPUT_DIR" -G "$MAKEFILES_GENERATOR" $CMAKE_ARGS
+apt-get install -y tree
 tree "$OUTPUT_DIR"
 
 if [ "$CMAKE_CONFIG_ONLY" = "false" ]; then
